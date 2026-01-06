bdb9ce33e23e47008a960b0098ed3de2bdaccd24
umer <syedumerahmedcode@gmail.com>
2026-01-06 18:42:30 +0100

    Feature: CRUD Update - updating the UI

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,15 @@
 import {
+  afterNextRender,
+  ChangeDetectorRef,
   Component,
   computed,
   effect,
+  EffectRef,
+  ElementRef,
   inject,
   Injector,
   signal,
+  viewChild,
 } from '@angular/core';
 import { CoursesService } from '../services/courses.service';
 import { Course, sortCoursesBySeqNo } from '../models/course.model';
@@ -12,7 +17,14 @@ import { MatTab, MatTabGroup } from '@angular/material/tabs';
 import { CoursesCardListComponent } from '../courses-card-list/courses-card-list.component';
 import { MatDialog } from '@angular/material/dialog';
 import { MessagesService } from '../messages/messages.service';
-import { catchError, from, throwError } from 'rxjs';
+import {
+  catchError,
+  from,
+  interval,
+  Observable,
+  startWith,
+  throwError,
+} from 'rxjs';
 import {
   toObservable,
   toSignal,
@@ -20,6 +32,8 @@ import {
   outputFromObservable,
 } from '@angular/core/rxjs-interop';
 import { CoursesServiceWithFetch } from '../services/courses-fetch.service';
+import { openEditCourseDialog } from '../edit-course-dialog/edit-course-dialog.component';
+import { LoadingService } from '../loading/loading.service';
 
 @Component({
   selector: 'home',
@@ -28,45 +42,125 @@ import { CoursesServiceWithFetch } from '../services/courses-fetch.service';
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-  //Adding # before a signal makes it private.
   #courses = signal<Course[]>([]);
 
-  //   coursesService = inject(CoursesServiceWithFetch);
   coursesService = inject(CoursesService);
 
+  dialog = inject(MatDialog);
+
   beginnerCourses = computed(() => {
-    // All dependencies of the signal must come at the start of the signal
     const courses = this.#courses();
     return courses.filter((course) => course.category === 'BEGINNER');
   });
 
   advancedCourses = computed(() => {
-    // All dependencies of the signal must come at the start of the signal
     const courses = this.#courses();
     return courses.filter((course) => course.category === 'ADVANCED');
   });
 
+  messageService = inject(MessagesService);
+
+  beginnersList = viewChild<CoursesCardListComponent>('beginnersList');
+
   constructor() {
     effect(() => {
-      console.log(`Begineer courses:`, this.beginnerCourses());
-      console.log(`Advanced courses:`, this.advancedCourses());
+      console.log(`beginnersList: `, this.beginnersList());
+    });
+
+    effect(() => {
+      console.log(`Beginner courses: `, this.beginnerCourses());
+      console.log(`Advanced courses: `, this.advancedCourses());
     });
 
     this.loadCourses().then(() =>
-      console.log(`All courses loaded: `, this.#courses())
+      console.log(`All courses loaded:`, this.#courses())
     );
   }
 
   async loadCourses() {
-    // this.coursesService
-    //   .loadAllCourses()
-    //   .then((courses) => this.courses.set(courses));
     try {
       const courses = await this.coursesService.loadAllCourses();
       this.#courses.set(courses.sort(sortCoursesBySeqNo));
     } catch (err) {
-      alert('Error loading courses');
+      //this.messageService.showMessage(`Error loading courses!`, 'error');
       console.error(err);
+    }
+  }
+
+  onCourseUpdated(updatedCourse: Course) {
+    const courses = this.#courses();
+    const newCourses = courses.map((course) =>
+      course.id === updatedCourse.id ? updatedCourse : course
+    );
+    this.#courses.set(newCourses);
+  }
+
+  async onCourseDeleted(courseId: string) {
+    try {
+      await this.coursesService.deleteCourse(courseId);
+      const courses = this.#courses();
+      const newCourses = courses.filter((course) => course.id !== courseId);
+      this.#courses.set(newCourses);
+    } catch (err) {
+      console.error(err);
+      alert(`Error deleting course.`);
+    }
+  }
+
+  async onAddCourse() {
+    const newCourse = await openEditCourseDialog(this.dialog, {
+      mode: 'create',
+      title: 'Create New Course',
+    });
+    if (!newCourse) {
+      return;
+    }
+    const newCourses = [...this.#courses(), newCourse];
+    this.#courses.set(newCourses);
+  }
+
+  onToObservableExample() {
+    const numbers = signal(0);
+    numbers.set(1);
+    numbers.set(2);
+    numbers.set(3);
+    const numbers$ = toObservable(numbers, {
+      injector: this.injector,
+    });
+    numbers.set(4);
+    numbers$.subscribe((val) => {
+      console.log(`numbers$: `, val);
+    });
+    numbers.set(5);
+  }
+
+  injector = inject(Injector);
+
+  onToSignalExample() {
+    try {
+      const courses$ = from(this.coursesService.loadAllCourses()).pipe(
+        catchError((err) => {
+          console.log(`Error caught in catchError`, err);
+          throw err;
+        })
+      );
+      const courses = toSignal(courses$, {
+        injector: this.injector,
+      });
+      effect(
+        () => {
+          console.log(`Courses: `, courses());
+        },
+        {
+          injector: this.injector,
+        }
+      );
+
+      setInterval(() => {
+        console.log(`Reading courses signal: `, courses());
+      }, 1000);
+    } catch (err) {
+      console.log(`Error in catch block: `, err);
     }
   }
 }
